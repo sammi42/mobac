63eb38b93532e4277d262c60a71e59e2121cf083
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2009-03-26 15:22:51 +0000

    more or less self written PNG writer that creates 4 bit png files (4 bit per pixel).
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@201 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

---
+++ src/tac/utilities/Png4BitWriter.java
@@ -0,0 +1,153 @@
+package tac.utilities;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.IndexColorModel;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedOutputStream;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+
+/**
+ * 4 Bit PNG Writer
+ * <p>
+ * Writes a color png image with pallette containing 16 colors. Currently the
+ * image data is saved without any PNG filtering.
+ * </p>
+ * 
+ * Bases on the PNGWriter written by Matthias Mann - www.matthiasmann.de
+ */
+public class Png4BitWriter {
+
+	private static final byte[] SIGNATURE = { (byte) 137, 80, 78, 71, 13, 10, 26, 10 };
+	private static final int IHDR = (int) 0x49484452;
+	private static final int IDAT = (int) 0x49444154;
+	private static final int IEND = (int) 0x49454E44;
+	private static final int PLTE = (int) 0x504C5445;
+
+	private static final byte COLOR_PALETTE = 3;
+
+	private static final byte COMPRESSION_DEFLATE = 0;
+	private static final byte INTERLACE_NONE = 0;
+
+	public static final byte FILTER_NONE = 0;
+	public static final byte FILTER_PAETH = 4;
+
+	/**
+	 * 
+	 * @param out
+	 * @param image
+	 *            Must be an image with {@link ColorModel}
+	 *            {@link IndexColorModel}
+	 * @throws IOException
+	 */
+	public static void writeImage(OutputStream out, BufferedImage image) throws IOException {
+		int[] pixels = null;
+		pixels = image.getData().getPixels(0, 0, image.getWidth(), image.getHeight(), pixels);
+		DataOutputStream dos = new DataOutputStream(out);
+
+		int width = image.getWidth();
+		int height = image.getHeight();
+
+		ColorModel cm = image.getColorModel();
+
+		if (!(cm instanceof IndexColorModel))
+			throw new UnsupportedOperationException("Image format not compatible");
+
+		IndexColorModel palette = (IndexColorModel) cm;
+
+		System.out.println(image.getWidth() + " x " + image.getHeight() + " = "
+				+ (image.getHeight() * image.getWidth()));
+		System.out.println(pixels.length);
+		dos.write(SIGNATURE);
+		Chunk cIHDR = new Chunk(IHDR);
+		cIHDR.writeInt(width);
+		cIHDR.writeInt(height);
+		cIHDR.writeByte(4); // 4 bit per component
+		cIHDR.writeByte(COLOR_PALETTE);
+		cIHDR.writeByte(COMPRESSION_DEFLATE);
+		cIHDR.writeByte(FILTER_NONE);
+		cIHDR.writeByte(INTERLACE_NONE);
+		cIHDR.writeTo(dos);
+
+		Chunk cPLTE = new Chunk(PLTE);
+		int paletteEntries = palette.getMapSize();
+		byte[] r = new byte[paletteEntries];
+		byte[] g = new byte[paletteEntries];
+		byte[] b = new byte[paletteEntries];
+		palette.getReds(r);
+		palette.getGreens(g);
+		palette.getBlues(b);
+		for (int i = 0; i < 16; i++) {
+			cPLTE.writeByte(r[i]);
+			cPLTE.writeByte(g[i]);
+			cPLTE.writeByte(b[i]);
+		}
+		cPLTE.writeTo(dos);
+
+		Chunk cIDAT = new Chunk(IDAT);
+		DeflaterOutputStream dfos = new DeflaterOutputStream(cIDAT, new Deflater(
+				Deflater.BEST_COMPRESSION));
+
+		int lineLen = MyMath.divCeil(width, 2);
+		byte[] lineOut = new byte[lineLen];
+		int[] samples = null;
+		
+		for (int line = 0; line < height; line++) {
+			dfos.write(FILTER_NONE);
+
+			// Get the samples for the next line - each byte is one sample/pixel
+			samples = image.getRaster().getPixels(0, line, width, 1, samples);
+			int sx = 0;
+			for (int i = 0; i < samples.length; i += 2) {
+				// Now we are packing two samples of 4 bit into one byte
+				lineOut[sx++] = (byte) (((samples[i] & 0x0F) << 4) | (samples[i + 1] & 0x0F));
+			}
+			dfos.write(lineOut);
+		}
+
+		dfos.finish();
+		cIDAT.writeTo(dos);
+
+		Chunk cIEND = new Chunk(IEND);
+		cIEND.writeTo(dos);
+
+		dos.flush();
+	}
+
+	protected static void writeColor(DataOutputStream dos, Color c) throws IOException {
+		dos.writeByte(c.getRed());
+		dos.writeByte(c.getGreen());
+		dos.writeByte(c.getBlue());
+	}
+
+	static class Chunk extends DataOutputStream {
+		final CRC32 crc;
+		final ByteArrayOutputStream baos;
+
+		Chunk(int chunkType) throws IOException {
+			this(chunkType, new ByteArrayOutputStream(), new CRC32());
+		}
+
+		private Chunk(int chunkType, ByteArrayOutputStream baos, CRC32 crc) throws IOException {
+			super(new CheckedOutputStream(baos, crc));
+			this.crc = crc;
+			this.baos = baos;
+
+			writeInt(chunkType);
+		}
+
+		public void writeTo(DataOutputStream out) throws IOException {
+			flush();
+			out.writeInt(baos.size() - 4);
+			baos.writeTo(out);
+			out.writeInt((int) crc.getValue());
+		}
+	}
+
+}

d900371b55e3fde22a57a0d5108462be71cd2de3
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2009-06-04 08:36:33 +0000

    version check added
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@344 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- src/tac/StartTAC.java
+++ src/tac/StartTAC.java
@@ -1,5 +1,6 @@
 package tac;
 
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -23,10 +24,34 @@ public class StartTAC {
 		} catch (Exception e) {
 			log.error("The selection of look and feel failed!", e);
 		}
+		checkVersion();
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				MainGUI.createMainGui();
 			}
 		});
+	}
+
+	protected static void checkVersion() {
+		String ver = System.getProperty("java.specification.version");
+		if (ver == null)
+			ver = "Unknown";
+		String[] v = ver.split("\\.");
+		int major = 0;
+		int minor = 0;
+		try {
+			major = Integer.parseInt(v[0]);
+			minor = Integer.parseInt(v[1]);
+		} catch (Exception e) {
+		}
+		int version = (major * 1000) + minor;
+		// 1.5 -> 1005; 1.6 -> 1006; 1.7 -> 1007
+		if (version < 1006)
+			JOptionPane.showMessageDialog(null,
+					"The used Java Runtime Environment does not meet the minimum requirements.\n"
+							+ "TrekBuddy Atlas Creator requires at least Java 1.6 or higher.\n"
+							+ "Otherwise several functions will not work properly.\n\n"
+							+ "Detected Java Runtime Version: " + ver,
+					"Java Runtime version problem detected", JOptionPane.WARNING_MESSAGE);
 	}
 }

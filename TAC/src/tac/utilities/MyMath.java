df53c8bbf62dd0a4291a25d7dc64c2ea69be2b4e
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2009-08-09 19:54:49 +0000

    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@455 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- TAC/src/tac/utilities/MyMath.java
+++ TAC/src/tac/utilities/MyMath.java
@@ -6,9 +6,93 @@ public class MyMath {
 		return (int) Math.ceil(value);
 	}
 
+	/**
+	 * Divides <code>value</code> by <code>divisor</code> and performs the
+	 * function "ceil" on the fractional result. <b>Warning: This method works
+	 * only with positive input values!</b>
+	 * <p>
+	 * Internally only integer arithmetics is used (no floating point
+	 * arithmetics)
+	 * </p>
+	 * 
+	 * @param value
+	 *            positive value
+	 * @param divisor
+	 *            positive value
+	 * @return
+	 */
 	public static int divCeil(int value, int divisor) {
 		int result = value / divisor;
+		int remainder = value % divisor;
+		if (remainder != 0)
+			result++;
+		return result;
+	}
+
+	/**
+	 * Divides <code>value</code> by <code>divisor</code> and performs the
+	 * function "ceil" on the fractional result.<b>Warning: This method works
+	 * only with positive input values!</b>
+	 * <p>
+	 * Internally only integer arithmetics is used (no floating point
+	 * arithmetics)
+	 * </p>
+	 * 
+	 * @param value
+	 *            positive value
+	 * @param divisor
+	 *            positive value
+	 * @return
+	 */
+	public static long divCeil(long value, long divisor) {
+		long result = value / divisor;
 		if (value % divisor != 0)
+			result++;
+		return result;
+	}
+
+	/**
+	 * Divides <code>value</code> by <code>divisor</code> and performs the
+	 * function "round" on the fractional result. <b>Warning: This method works
+	 * only with positive input values!</b>
+	 * <p>
+	 * Internally only integer arithmetics is used (no floating point
+	 * arithmetics)
+	 * </p>
+	 * 
+	 * @param value
+	 *            positive value
+	 * @param divisor
+	 *            positive value
+	 * @return
+	 */
+	public static int divRound(int value, int divisor) {
+		int result = value / divisor;
+		int remainder = value % divisor;
+		if (remainder > (divisor >> 1))
+			result++;
+		return result;
+	}
+
+	/**
+	 * Divides <code>value</code> by <code>divisor</code> and performs the
+	 * function "round" on the fractional result. <b>Warning: This method works
+	 * only with positive input values!</b>
+	 * <p>
+	 * Internally only integer arithmetics is used (no floating point
+	 * arithmetics)
+	 * </p>
+	 * 
+	 * @param value
+	 *            positive value
+	 * @param divisor
+	 *            positive value
+	 * @return
+	 */
+	public static long divRound(long value, long divisor) {
+		long result = value / divisor;
+		long remainder = value % divisor;
+		if (remainder > (divisor >> 1))
 			result++;
 		return result;
 	}

103439deb35659a65db48d8ea9d376644caabc5c
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2010-01-11 21:29:24 +0000

    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@891 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- experiments/src/geotiff/Gt2GeoTiffTest.java
+++ experiments/src/geotiff/Gt2GeoTiffTest.java
@@ -1,8 +1,13 @@
 package geotiff;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 import java.io.File;
-import java.io.IOException;
 
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.coverage.grid.GridCoverageFactory;
+import org.geotools.coverage.grid.GridGeometry2D;
 import org.geotools.gce.geotiff.GeoTiffWriter;
 
 public class Gt2GeoTiffTest {
@@ -12,8 +17,13 @@ public class Gt2GeoTiffTest {
 	 */
 	public static void main(String[] args) {
 		try {
+	        BufferedImage img = new BufferedImage(1024,1024,BufferedImage.TYPE_INT_RGB);
+			GridCoverageFactory factory = CoverageFactoryFinder.getGridCoverageFactory(null);
+			GridGeometry2D gridGeo;
+			GridCoverage2D cov = factory.create("Coverage", img, gridGeo);
 			GeoTiffWriter writer = new GeoTiffWriter(new File("test,tiff"));
-		} catch (IOException e) {
+			writer.write(cov, null);
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}

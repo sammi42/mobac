01519daa72f8a7e7218b016b1af6f27629c5fe8e
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2010-02-04 19:02:31 +0000

    polygonal map painting added
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1009 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- MOBAC/src/mobac/gui/mapview/MultiMapSelectionLayer.java
+++ MOBAC/src/mobac/gui/mapview/MultiMapSelectionLayer.java
@@ -2,15 +2,16 @@ package mobac.gui.mapview;
 
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Polygon;
 
 import mobac.program.interfaces.AtlasInterface;
 import mobac.program.interfaces.AtlasObject;
 import mobac.program.interfaces.LayerInterface;
 import mobac.program.interfaces.MapInterface;
+import mobac.program.model.MapPolygon;
 
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 import org.openstreetmap.gui.jmapviewer.interfaces.MapLayer;
-
 
 public class MultiMapSelectionLayer implements MapLayer {
 
@@ -39,25 +40,21 @@ public class MultiMapSelectionLayer implements MapLayer {
 
 	protected void paintMap(MapInterface map, Graphics2D g, int zoom, int minX, int minY, int maxX,
 			int maxY) {
+		if (map instanceof MapPolygon)
+			paintMapPolygon((MapPolygon) map, g, zoom, minX, minY, maxX, maxY);
+		else
+			paintMapRectangle(map, g, zoom, minX, minY, maxX, maxY);
+	}
+
+	protected void paintMapRectangle(MapInterface map, Graphics2D g, int zoom, int minX, int minY,
+			int maxX, int maxY) {
 		Point max = map.getMaxTileCoordinate();
 		Point min = map.getMinTileCoordinate();
 		int zoomDiff = map.getZoom() - zoom;
-		int mapX = min.x;
-		int mapY = min.y;
-		int mapW = max.x - min.x + 1;
-		int mapH = max.y - min.y + 1;
-		if (zoomDiff > 0) {
-			mapX >>= zoomDiff;
-			mapY >>= zoomDiff;
-			mapW >>= zoomDiff;
-			mapH >>= zoomDiff;
-		} else {
-			zoomDiff = Math.abs(zoomDiff);
-			mapX <<= zoomDiff;
-			mapY <<= zoomDiff;
-			mapW <<= zoomDiff;
-			mapH <<= zoomDiff;
-		}
+		int mapX = applyZoomDiff(min.x, zoomDiff);
+		int mapY = applyZoomDiff(min.y, zoomDiff);
+		int mapW = applyZoomDiff(max.x - min.x + 1, zoomDiff);
+		int mapH = applyZoomDiff(max.y - min.y + 1, zoomDiff);
 		int x = mapX - minX;
 		int y = mapY - minY;
 		int w = mapW;
@@ -66,6 +63,25 @@ public class MultiMapSelectionLayer implements MapLayer {
 		g.fillRect(x, y, w, h);
 		g.setColor(PreviewMap.GRID_COLOR);
 		g.drawRect(x, y, w, h);
+	}
+
+	protected void paintMapPolygon(MapPolygon map, Graphics2D g, int zoom, int minX, int minY,
+			int maxX, int maxY) {
+		Polygon p = map.getPolygon();
+		int zoomDiff = map.getZoom() - zoom;
+
+		int[] px = new int[p.npoints];
+		int[] py = new int[p.npoints];
+		for (int i = 0; i < px.length; i++) {
+			px[i] = applyZoomDiff(p.xpoints[i], zoomDiff) - minX;
+			py[i] = applyZoomDiff(p.ypoints[i], zoomDiff) - minY;
+		}
+		g.setColor(PreviewMap.MAP_COLOR);
+		g.fillPolygon(px, py, px.length);
+	}
+
+	private static int applyZoomDiff(int pixelCoord, int zoomDiff) {
+		return (zoomDiff > 0) ? pixelCoord >> zoomDiff : pixelCoord << -zoomDiff;
 	}
 
 	@Override

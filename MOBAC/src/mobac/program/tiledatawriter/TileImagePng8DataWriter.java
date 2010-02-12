f32245e037852f5a1917ebeb77dacf3bfffb25e5
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2010-02-12 12:08:35 +0000

    BugFix #2950505 "PNG 256 creation does not work"
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1042 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- MOBAC/src/mobac/program/tiledatawriter/TileImagePng8DataWriter.java
+++ MOBAC/src/mobac/program/tiledatawriter/TileImagePng8DataWriter.java
@@ -1,20 +1,20 @@
 package mobac.program.tiledatawriter;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.IOException;
 import java.io.OutputStream;
 
 import mobac.optional.JavaAdvancedImaging;
-
 
 public class TileImagePng8DataWriter extends TileImagePngDataWriter {
 
 	public TileImagePng8DataWriter() {
 	}
 
-	public void processImage(RenderedImage image, OutputStream out) throws IOException {
+	@Override
+	public void processImage(BufferedImage image, OutputStream out) throws IOException {
 		BufferedImage image2 = JavaAdvancedImaging.colorReduceMedianCut(image, 256);
+		System.out.println(image2.getColorModel());
 		super.processImage(image2, out);
 	}
 

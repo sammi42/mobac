af30d6c25dc8d768cef2cfe0bab6ea3d1dfd0baf
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2010-04-04 13:42:28 +0000

    Internal structure changed for OSMTracker based atlas creators (preparation for PathAway atlas creator)
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1099 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- MOBAC/src/mobac/program/atlascreators/Maverick.java
+++ MOBAC/src/mobac/program/atlascreators/Maverick.java
@@ -1,19 +1,15 @@
 package mobac.program.atlascreators;
 
-import mobac.program.interfaces.MapInterface;
-import mobac.utilities.tar.TarIndex;
-
 /**
  * Creates maps using the <a
  * href="http://www.codesector.com/maverick.php">Maverick</a> atlas format
  * (Android application).
  */
-public class Maverick extends AndNav {
+public class Maverick extends OSMTracker {
 
-	@Override
-	public void initializeMap(MapInterface map, TarIndex tarTileIndex) {
-		super.initializeMap(map, tarTileIndex);
-		this.additionalFileExt = ".tile";
+	public Maverick() {
+		super();
+		tileFileNamePattern += ".tile";
 	}
 
 }
--- MOBAC/src/mobac/program/atlascreators/OSMTracker.java
+++ MOBAC/src/mobac/program/atlascreators/OSMTracker.java
@@ -1,17 +1,86 @@
 package mobac.program.atlascreators;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import mobac.exceptions.MapCreationException;
+import mobac.mapsources.mapspace.MercatorPower2MapSpace;
+import mobac.program.atlascreators.tileprovider.ConvertedRawTileProvider;
 import mobac.program.interfaces.MapInterface;
+import mobac.utilities.Utilities;
 import mobac.utilities.tar.TarIndex;
 
+import org.openstreetmap.gui.jmapviewer.interfaces.MapSource;
+
 /**
- * Created by didoa [at] users.sourceforge.net
+ * Creates maps identical to the atlas format used by OSMTracker.
+ * 
+ * Please note that this atlas format ignores the defined atlas structure. It
+ * uses a separate directory for each used map source and inside one directory
+ * for each zoom level.
  */
-public class OSMTracker extends AndNav {
+public class OSMTracker extends AtlasCreator {
+
+	protected File mapDir = null;
+
+	protected String tileFileNamePattern = "%d/%d/%d.%s";
+
+	protected String tileType = null;
+
+	@Override
+	public boolean testMapSource(MapSource mapSource) {
+		return MercatorPower2MapSpace.INSTANCE_256.equals(mapSource.getMapSpace());
+	}
 
 	@Override
 	public void initializeMap(MapInterface map, TarIndex tarTileIndex) {
 		super.initializeMap(map, tarTileIndex);
-		this.additionalFileExt = "";
+		mapDir = new File(atlasDir, map.getMapSource().getName());
+		tileType = mapSource.getTileType();
+		if (parameters != null) {
+			mapDlTileProvider = new ConvertedRawTileProvider(mapDlTileProvider, parameters
+					.getFormat());
+			tileType = parameters.getFormat().getDataWriter().getFileExt();
+		}
+	}
+
+	public void createMap() throws MapCreationException, InterruptedException {
+		// This means there should not be any resizing of the tiles.
+		createTiles();
+	}
+
+	protected void createTiles() throws InterruptedException, MapCreationException {
+		atlasProgress.initMapCreation((xMax - xMin + 1) * (yMax - yMin + 1));
+		ImageIO.setUseCache(false);
+
+		for (int x = xMin; x <= xMax; x++) {
+			for (int y = yMin; y <= yMax; y++) {
+				checkUserAbort();
+				atlasProgress.incMapCreationProgress();
+				try {
+					byte[] sourceTileData = mapDlTileProvider.getTileData(x, y);
+					if (sourceTileData != null) {
+						File f = getTileFile(x, y, zoom);
+						Utilities.mkDirs(f.getParentFile());
+						FileOutputStream out = new FileOutputStream(f);
+						try {
+							out.write(sourceTileData);
+						} finally {
+							Utilities.closeStream(out);
+						}
+					}
+				} catch (IOException e) {
+					throw new MapCreationException("Error writing tile image: " + e.getMessage(), e);
+				}
+			}
+		}
+	}
+
+	protected File getTileFile(int x, int y, int zoom) {
+		return new File(mapDir, String.format(tileFileNamePattern, zoom, x, y, tileType));
 	}
 
 }

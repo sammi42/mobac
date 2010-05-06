e7a8de17cb7fc84aa74a8e023860be545756be34
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2010-05-06 12:12:56 +0000

    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1144 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- MOBAC/src/mobac/program/atlascreators/SQLitexGPS.java
+++ MOBAC/src/mobac/program/atlascreators/SQLitexGPS.java
@@ -5,13 +5,15 @@ import java.sql.Statement;
 
 public class SQLitexGPS extends BigPlanetSql {
 
-	private static final String TABLE_TILES = "CREATE TABLE tiles "
-			+ "(x INTEGER, y INTEGER,zoom INTEGER,type INTEGER, img BLOB, PRIMARY KEY(x,y,zoom,type))";
+	private static final String TABLE_MAPS = "CREATE TABLE maps "
+			+ "(id INTEGER, name TEXT, zoom TEXT, type INTEGER, PRIMARY KEY(id));";
 	private static final String TABLE_MAPREGIONS = "CREATE TABLE map_regions "
 			+ "(regionsid INTEGER, mapid INTEGER, PRIMARY KEY(regionsid))";
-	private static final String REGIONS_POINTS = "CREATE TABLE regions_points "
+	private static final String TABLE_REGIONS_POINTS = "CREATE TABLE regions_points "
 			+ "(regionsid INTEGER, lat REAL,lon REAL, pos INTEGER, PRIMARY KEY(regionsid,lat,lon))";
-	
+	private static final String TABLE_TILES = "CREATE TABLE tiles "
+			+ "(x INTEGER, y INTEGER,zoom INTEGER,type INTEGER, img BLOB, PRIMARY KEY(x,y,zoom,type))";
+
 	private static final String INSERT_SQL = "INSERT or REPLACE INTO tiles "
 			+ "(x,y,zoom,type,img) VALUES (?,?,?,?,?)";
 	private static final String DATABASE_FILENAME = "xGPS_map.db";
@@ -19,9 +21,10 @@ public class SQLitexGPS extends BigPlanetSql {
 	@Override
 	protected void initializeDB() throws SQLException {
 		Statement stat = conn.createStatement();
+		stat.executeUpdate(TABLE_MAPS);
 		stat.executeUpdate(TABLE_TILES);
 		stat.executeUpdate(TABLE_MAPREGIONS);
-		stat.executeUpdate(REGIONS_POINTS);
+		stat.executeUpdate(TABLE_REGIONS_POINTS);
 		stat.close();
 	}
 

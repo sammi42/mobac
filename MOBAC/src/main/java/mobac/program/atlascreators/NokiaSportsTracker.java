192c52257627bfba1eec2c3695d0c7bcb3616488
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2010-07-07 14:12:39 +0000

    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1207 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

---
+++ MOBAC/src/main/java/mobac/program/atlascreators/NokiaSportsTracker.java
@@ -0,0 +1,108 @@
+package mobac.program.atlascreators;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import mobac.exceptions.AtlasTestException;
+import mobac.exceptions.MapCreationException;
+import mobac.mapsources.mapspace.MercatorPower2MapSpace;
+import mobac.program.atlascreators.impl.MapTileWriter;
+import mobac.program.atlascreators.tileprovider.ConvertedRawTileProvider;
+import mobac.program.interfaces.LayerInterface;
+import mobac.program.interfaces.MapInterface;
+import mobac.program.model.TileImageFormat;
+import mobac.program.model.TileImageParameters;
+import mobac.utilities.Utilities;
+import mobac.utilities.tar.TarIndex;
+
+import org.openstreetmap.gui.jmapviewer.interfaces.MapSource;
+
+public class NokiaSportsTracker extends AtlasCreator {
+
+	protected File mapDir = null;
+
+	protected MapTileWriter mapTileWriter = null;
+
+	@Override
+	public boolean testMapSource(MapSource mapSource) {
+		return MercatorPower2MapSpace.INSTANCE_256.equals(mapSource.getMapSpace());
+	}
+
+	@Override
+	protected void testAtlas() throws AtlasTestException {
+		for (LayerInterface layer : atlas) {
+			for (MapInterface map : layer) {
+				TileImageParameters param = map.getParameters();
+				if (param != null)
+					throw new AtlasTestException("Custom tile settings are not supported by this atlas format");
+			}
+		}
+	}
+
+	@Override
+	public void initializeMap(MapInterface map, TarIndex tarTileIndex) {
+		super.initializeMap(map, tarTileIndex);
+		mapDir = new File(atlasDir, map.getName());
+		if (!"png".equalsIgnoreCase(mapSource.getTileType()))
+			// If the tile image format is not png we have to convert it
+			mapDlTileProvider = new ConvertedRawTileProvider(mapDlTileProvider, TileImageFormat.PNG);
+	}
+
+	public void createMap() throws MapCreationException, InterruptedException {
+		// This means there should not be any resizing of the tiles.
+		createTiles();
+	}
+
+	protected void createTiles() throws InterruptedException, MapCreationException {
+		atlasProgress.initMapCreation((xMax - xMin + 1) * (yMax - yMin + 1));
+		ImageIO.setUseCache(false);
+
+		for (int x = xMin; x <= xMax; x++) {
+			for (int y = yMin; y <= yMax; y++) {
+				checkUserAbort();
+				atlasProgress.incMapCreationProgress();
+				try {
+					byte[] sourceTileData = mapDlTileProvider.getTileData(x, y);
+					if (sourceTileData != null)
+						writeTile(x, y, sourceTileData);
+				} catch (IOException e) {
+					throw new MapCreationException("Error writing tile image: " + e.getMessage(), e);
+				}
+			}
+		}
+	}
+
+	public void writeTile(int tilex, int tiley, byte[] tileData) throws IOException {
+		String fileName = getTileName(zoom, tilex, tiley) + ".jpg";
+		File file = new File(mapDir, fileName);
+		writeTile(file, tileData);
+	}
+
+	protected void writeTile(File file, byte[] tileData) throws IOException {
+		Utilities.mkDirs(file.getParentFile());
+		FileOutputStream out = new FileOutputStream(file);
+		try {
+			out.write(tileData);
+		} finally {
+			Utilities.closeStream(out);
+		}
+	}
+
+	protected static final char[] NUM_CHAR = { 'q', 'r', 't', 's' };
+
+	public static String getTileName(int zoom, int tilex, int tiley) {
+		char[] tileNum = new char[zoom + 1];
+		tileNum[0] = 't';
+		for (int i = zoom; i > 0; i--) {
+			int num = (tilex % 2) | ((tiley % 2) << 1);
+			tileNum[i] = NUM_CHAR[num];
+			tilex >>= 1;
+			tiley >>= 1;
+		}
+		return new String(tileNum);
+	}
+
+}

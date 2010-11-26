fdf4d9a0e0f033db4c501d2db0751d12eb7eacc2
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2010-11-26 13:17:37 +0000

    further optimizations
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1385 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- MOBAC/src/main/java/mobac/program/atlascreators/MGM.java
+++ MOBAC/src/main/java/mobac/program/atlascreators/MGM.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
  * Copyright (c) MOBAC developers
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -20,11 +20,7 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -51,12 +47,23 @@ import mobac.utilities.tar.TarIndex;
  * 
  * Each zoom level in a different directory, 64 tiles per mgm file.
  * 
+ * <h3>Format</h3>
+ * <ul>
+ * <li>2 bytes: number of tiles in this file for each tile (even for tiles that are not used, in which case the data is
+ * left null)</li>
+ * <li>1 byte: tile x within this file; add tileX * tilesPerFileX to get global tile number</li>
+ * <li>1 byte: tile y within this file; add tileY * tilesPerFileY to get global tile number</li>
+ * <li>4 bytes: offset of the end of the tile data within this file (to get the offset for the start of the tile data,
+ * subtract the value for the previous tile, or 2 + 6 * tilesPerFile tile data 1 tile data 2 ...</li>
+ * </ul>
+ * 
  * @author paour
  */
 public class MGM extends AtlasCreator {
 
-	private static final int TILES_PER_FILE = 64;
 	private static final int TILES_PER_FILE_X = 8;
+	private static final int TILES_PER_FILE_Y = 8;
+	private static final int TILES_PER_FILE = TILES_PER_FILE_X * TILES_PER_FILE_Y;
 
 	private double xResizeRatio = 1.0;
 	private double yResizeRatio = 1.0;
@@ -99,51 +106,23 @@ public class MGM extends AtlasCreator {
 
 	@Override
 	public void createMap() throws MapCreationException, InterruptedException {
+		MGMTileWriter mgmTileWriter = null;
 		try {
-			StringBuilder name = new StringBuilder(map.getLayer().getName());
+			if ((xResizeRatio != 1.0) || (yResizeRatio != 1.0))
+				mgmTileWriter = new MGMResizeTileWriter();
+			else
+				mgmTileWriter = new MGMTileWriter();
 
-			// safe name
-			for (int i = 0; i < name.length(); i++) {
-				char c = name.charAt(i);
-				if ((c < '0' || c > '9') && (c < 'A' || c > 'Z') && (c < 'a' || c > 'z')) {
-					name.setCharAt(i, '_');
-				}
-			}
+			String name = map.getLayer().getName();
+
+			// safe naming: replace all non-word characters: [^a-zA-Z_0-9]
+			name = name.replaceAll("[^a-zA-Z_0-9]", "_");
 
 			// crate directory if necessary
 			File folder = new File(atlasDir, name + "_" + map.getZoom());
-			folder.mkdirs();
+			Utilities.mkDirs(folder);
 
 			atlasProgress.initMapCreation((xMax - xMin + 1) * (yMax - yMin + 1));
-
-			// number of tiles for this zoom level
-			// final long nbTotalTiles = (256 * Math.round(Math.pow(2, map.getZoom()))) / tileSize;
-
-			// tile resizing
-			BufferedImage tileImage = null;
-			Graphics2D graphics = null;
-			ArrayOutputStream buffer = null;
-			TileImageDataWriter writer = null;
-			boolean resizeImage = false;
-
-			if ((xResizeRatio != 1.0) || (yResizeRatio != 1.0)) {
-				// resize image
-				tileImage = new BufferedImage(parameters.getWidth(), parameters.getHeight(),
-						BufferedImage.TYPE_3BYTE_BGR);
-
-				// associated graphics with affine transform
-				graphics = tileImage.createGraphics();
-				graphics.setTransform(AffineTransform.getScaleInstance(xResizeRatio, yResizeRatio));
-				graphics
-						.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-
-				// image compression writer
-				writer = parameters.getFormat().getDataWriter();
-
-				// buffer to store compressed image
-				buffer = new ArrayOutputStream(3 * parameters.getWidth() * parameters.getHeight());
-				resizeImage = true;
-			}
 
 			ImageIO.setUseCache(false);
 
@@ -152,105 +131,61 @@ public class MGM extends AtlasCreator {
 			int pyMin = yMin / TILES_PER_FILE_X;
 			int pyMax = xMax / TILES_PER_FILE_X;
 
-			byte[] buf = new byte[32768];
-
 			for (int px = pxMin; px <= pxMax; px++) {
 				for (int py = pyMin; py <= pyMax; py++) {
 					int count = 0;
 					int pos = 2 + TILES_PER_FILE * 6;
-					File tmp = null;
-					BufferedOutputStream bostmp = null;
+					File pack = new File(folder, px + "_" + py + ".mgm");
 					RandomAccessFile raf = null;
-					BufferedInputStream bis = null;
+
 					try {
-						File pack = new File(folder, px + "_" + py + ".mgm");
-						tmp = new File(folder, px + "_" + py + ".mgm.tmp");
-						raf = new RandomAccessFile(pack, "rw");
-						bostmp = new BufferedOutputStream(new FileOutputStream(tmp));
-
-						// write a temporary count
-						raf.writeChar(TILES_PER_FILE);
-
 						for (int i = 0; i < TILES_PER_FILE_X; i++) {
 							int x = px * TILES_PER_FILE_X + i;
 							if (x < xMin || x > xMax) {
 								continue;
 							}
 
-							for (int j = 0; j < TILES_PER_FILE_X; j++) {
-								int y = py * TILES_PER_FILE_X + j;
+							for (int j = 0; j < TILES_PER_FILE_Y; j++) {
+								int y = py * TILES_PER_FILE_Y + j;
 								if (y < yMin || y > yMax) {
 									continue;
 								}
 
+								if (raf == null)
+									// Only create a file when needed
+									raf = new RandomAccessFile(pack, "rw");
+
 								checkUserAbort();
-
 								atlasProgress.incMapCreationProgress();
-
-								try {
-									// retrieve the tile data (already re-sampled if needed)
-									final BufferedImage tile = mapDlTileProvider.getTileImage(x, y);
-
-									byte[] sourceTileData;
-									if (resizeImage) {
-										// need to resize the tile
-										graphics.drawImage(tile, 0, 0, null);
-
-										buffer.reset();
-
-										writer.initialize();
-										writer.processImage(tileImage, buffer);
-
-										sourceTileData = buffer.toByteArray();
-
-										if (sourceTileData == null)
-											throw new MapCreationException("Image resizing failed.");
-									} else {
-										sourceTileData = mapDlTileProvider.getTileData(x, y);
-									}
-									if (sourceTileData != null) {
-										bostmp.write(sourceTileData);
-										count++;
-
-										raf.writeByte(i);
-										raf.writeByte(j);
-										pos += sourceTileData.length;
-										raf.writeInt(pos);
-									}
-								} catch (IOException e) {
-									throw new MapCreationException("Error writing tile image: " + e.getMessage(), e);
+								int res = mgmTileWriter.writeTile(x, y, i, j, raf, pos, count);
+								if (res >= 0) {
+									pos = res;
+									count++;
 								}
 							}
 						}
 
-						bostmp.close();
-						bostmp = null;
-
-						if (count != TILES_PER_FILE) {
+						if (raf != null) {
+							// POSITION 0: number of tiles
 							raf.seek(0);
 							raf.writeChar(count);
 						}
-
-						raf.seek(2 + TILES_PER_FILE * 6);
-
-						bis = new BufferedInputStream(new FileInputStream(tmp));
-						int len;
-						while ((len = bis.read(buf)) != -1) {
-							raf.write(buf, 0, len);
-						}
-
 					} finally {
-						Utilities.closeStream(bis);
 						Utilities.closeFile(raf);
-						Utilities.closeStream(bostmp);
-						tmp.delete();
+					}
+					if (count == 0) {
+						// the file doesn't contain any tiles
+						if (pack.exists())
+							Utilities.deleteFile(pack);
 					}
 				}
-				if (graphics != null)
-					graphics.dispose();
 			}
+
 		} catch (Exception e) {
 			throw new MapCreationException(e);
+		} finally {
+			if (mgmTileWriter != null)
+				mgmTileWriter.dispose();
 		}
 	}
 
@@ -262,4 +197,86 @@ public class MGM extends AtlasCreator {
 						.equals(mapSource.getMapSpace().getProjectionCategory()));
 	}
 
+	/**
+	 * Simply writes the tile to the file without resizing
+	 */
+	private class MGMTileWriter {
+
+		protected byte[] getSourceTileData(int x, int y) throws IOException {
+			return mapDlTileProvider.getTileData(x, y);
+		}
+
+		public int writeTile(int x, int y, int i, int j, RandomAccessFile raf, int startPos, int count)
+				throws MapCreationException {
+			try {
+				byte[] sourceTileData = getSourceTileData(x, y);
+				if (sourceTileData == null)
+					return -1;
+				raf.seek(startPos);
+				raf.write(sourceTileData);
+
+				// write the tile index
+				raf.seek(2 + count * 6);
+				raf.writeByte(i);
+				raf.writeByte(j);
+				int pos = startPos + sourceTileData.length;
+				raf.writeInt(pos);
+				return pos;
+			} catch (IOException e) {
+				throw new MapCreationException("Error writing tile image: " + e.getMessage(), e);
+			}
+		}
+
+		public void dispose() {
+			// Nothing to do
+		}
+	}
+
+	/**
+	 * Resizes the tile and saves it to the file
+	 */
+	private class MGMResizeTileWriter extends MGMTileWriter {
+
+		private final BufferedImage tileImage;
+		private final Graphics2D graphics;
+		private final TileImageDataWriter writer;
+		private final ArrayOutputStream buffer;
+
+		public MGMResizeTileWriter() {
+			// resize image
+			tileImage = new BufferedImage(parameters.getWidth(), parameters.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+
+			// associated graphics with affine transform
+			graphics = tileImage.createGraphics();
+			graphics.setTransform(AffineTransform.getScaleInstance(xResizeRatio, yResizeRatio));
+			graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+			// image compression writer
+			writer = parameters.getFormat().getDataWriter();
+
+			// buffer to store compressed image
+			buffer = new ArrayOutputStream(3 * parameters.getWidth() * parameters.getHeight());
+		}
+
+		@Override
+		protected byte[] getSourceTileData(int x, int y) throws IOException {
+			// need to resize the tile
+			final BufferedImage tile = mapDlTileProvider.getTileImage(x, y);
+			graphics.drawImage(tile, 0, 0, null);
+			buffer.reset();
+			writer.initialize();
+			writer.processImage(tileImage, buffer);
+
+			byte[] processedTileData = buffer.toByteArray();
+			buffer.reset();
+			return processedTileData;
+		}
+
+		@Override
+		public void dispose() {
+			buffer.reset();
+			graphics.dispose();
+		}
+
+	}
 }

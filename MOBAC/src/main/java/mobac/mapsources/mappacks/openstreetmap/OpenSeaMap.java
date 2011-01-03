b62465220f4104ed05c6154d8b9ab68b6d843020
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2011-01-03 13:29:52 +0000

    Switched to javapng library for OpenSeaMap
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1479 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- MOBAC/src/main/java/mobac/mapsources/mappacks/openstreetmap/OpenSeaMap.java
+++ MOBAC/src/main/java/mobac/mapsources/mappacks/openstreetmap/OpenSeaMap.java
@@ -1,12 +1,9 @@
 package mobac.mapsources.mappacks.openstreetmap;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.awt.Transparency;
 import java.awt.image.BufferedImage;
-import java.awt.image.ComponentColorModel;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageFilter;
 import java.awt.image.ImageProducer;
@@ -14,11 +11,6 @@ import java.awt.image.RGBImageFilter;
 import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.metadata.IIOMetadata;
-import javax.imageio.stream.MemoryCacheImageInputStream;
 
 import mobac.exceptions.TileException;
 import mobac.exceptions.UnrecoverableDownloadException;
@@ -30,7 +22,6 @@ import mobac.program.model.TileImageType;
 import mobac.program.tilestore.TileStore;
 import mobac.program.tilestore.TileStoreEntry;
 import mobac.utilities.Utilities;
-import mobac.utilities.imageio.PngConstants;
 
 /**
  *@see OpenSeaMapLayer
@@ -76,49 +67,12 @@ public class OpenSeaMap extends AbstractMultiLayerMapSource {
 		public BufferedImage getTileImage(int zoom, int x, int y, LoadMethod loadMethod) throws IOException,
 				UnrecoverableDownloadException, InterruptedException {
 			try {
-				ImageReader reader = ImageIO.getImageReadersByFormatName("png").next();
 				byte[] data = getTileData(zoom, x, y, loadMethod);
 				if (data == null) {
 					return null;
 				}
-				// if (1==1) return ImageIO.read(new ByteArrayInputStream(data));
-				reader.setInput(new MemoryCacheImageInputStream(new ByteArrayInputStream(data)), false, false);
-				BufferedImage image = reader.read(0);
-				if (image.getTransparency() == Transparency.OPAQUE
-						&& image.getColorModel() instanceof ComponentColorModel) {
-
-					Color transparencyColor = null;
-					IIOMetadata meta = reader.getImageMetadata(0);
-					if (meta instanceof com.sun.imageio.plugins.png.PNGMetadata) {
-						com.sun.imageio.plugins.png.PNGMetadata pngmeta;
-						pngmeta = (com.sun.imageio.plugins.png.PNGMetadata) meta;
-						if (!pngmeta.tRNS_present)
-							return image;
-						if (pngmeta.tRNS_colorType == PngConstants.COLOR_TRUECOLOR)
-							transparencyColor = new Color(pngmeta.tRNS_red, pngmeta.tRNS_green, pngmeta.tRNS_blue);
-						else if (pngmeta.tRNS_colorType == PngConstants.COLOR_GRAYSCALE) {
-							// transparencyColor = new Color(pngmeta.tRNS_gray, pngmeta.tRNS_gray, pngmeta.tRNS_gray);
-
-							// For an unknown reason the saved transparency color does not match the background color -
-							// therefore at the moment we use a hard coded transparency color
-							transparencyColor = new Color(0xfcfcfc);
-						}
-
-					} else
-						transparencyColor = new Color(248, 248, 248);
-					Image correctedImage = makeColorTransparent(image, transparencyColor);
-					BufferedImage image2 = new BufferedImage(image.getWidth(), image.getHeight(),
-							BufferedImage.TYPE_INT_ARGB);
-					Graphics2D g = image2.createGraphics();
-					try {
-						g.drawImage(correctedImage, 0, 0, null);
-						// g.setColor(Color.RED);
-						// g.drawRect(0, 0, image.getWidth() - 1, image.getHeight() - 1);
-						image = image2;
-					} finally {
-						g.dispose();
-					}
-				}
+				com.sixlegs.png.PngImage png = new com.sixlegs.png.PngImage();
+				BufferedImage image = png.read(new ByteArrayInputStream(data), true);
 				return image;
 			} catch (FileNotFoundException e) {
 				TileStore ts = TileStore.getInstance();

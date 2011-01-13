cbedb5d37f9708aad416c53108c9d455525c9437
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2011-01-13 12:37:57 +0000

    New map sources: ArcGIS topo map/satellite/street map Bugfix: xchanged mapsources.list of misc_worldwide and bing
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1503 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

---
+++ MOBAC/src/main/java/mobac/mapsources/mappacks/misc_worldwide/ArcGISTopoMap.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) MOBAC developers
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package mobac.mapsources.mappacks.misc_worldwide;
+
+import mobac.mapsources.AbstractHttpMapSource;
+import mobac.program.model.TileImageType;
+
+/**
+ * http://www.arcgis.com/home/webmap/viewer.html?useExisting=1
+ */
+public class ArcGISTopoMap extends AbstractHttpMapSource {
+
+	private static final String BASE_URL = "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/";
+
+	public ArcGISTopoMap() {
+		super("ArcGISTopoMap", 0, 19, TileImageType.JPG, TileUpdate.IfModifiedSince);
+	}
+
+	public String getTileUrl(int zoom, int x, int y) {
+		return BASE_URL + zoom + "/" + y + "/" + x;
+	}
+
+	@Override
+	public String toString() {
+		return "ArcGIS Topo Map";
+	}
+
+}

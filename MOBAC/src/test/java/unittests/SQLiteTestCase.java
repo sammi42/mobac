3b79930e39cbe4613d3985c6894117287de27a5b
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2011-01-22 14:15:31 +0000

    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1534 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- MOBAC/src/test/java/unittests/SQLiteTestCase.java
+++ MOBAC/src/test/java/unittests/SQLiteTestCase.java
@@ -17,17 +17,11 @@
 package unittests;
 
 import mobac.program.atlascreators.RMapsSQLite;
-import mobac.program.atlascreators.SQLitexGPS;
 
 public class SQLiteTestCase extends AbstractAtlasCreatorTestCase {
 
 	public SQLiteTestCase() {
 		super();
-	}
-
-	public void testXGPS() throws Exception {
-		log.info("Starting test testXGPS");
-		createAtlas("HamburgPark", SQLitexGPS.class);
 	}
 
 	public void testRMaps() throws Exception {

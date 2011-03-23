29ac96eaeb0048e5d89b886dee319ecf1febc6c4
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2011-03-23 19:30:03 +0000

    Default map source changed to MapQuest
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1681 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- MOBAC/src/main/java/mobac/mapsources/DefaultMapSourcesManager.java
+++ MOBAC/src/main/java/mobac/mapsources/DefaultMapSourcesManager.java
@@ -205,7 +205,7 @@ public class DefaultMapSourcesManager extends MapSourcesManager {
 
 	@Override
 	public MapSource getDefaultMapSource() {
-		MapSource ms = getSourceByName("Mapnik");// DEFAULT;
+		MapSource ms = getSourceByName("MapQuest");// DEFAULT;
 		if (ms != null)
 			return ms;
 		// Fallback: return first

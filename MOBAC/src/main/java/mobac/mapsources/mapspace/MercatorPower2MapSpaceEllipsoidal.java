e8e79f604f525a024dd5856e4d0534606086afc9
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2011-05-03 08:07:50 +0000

    Bugfix: equals() missing
    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1790 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- MOBAC/src/main/java/mobac/mapsources/mapspace/MercatorPower2MapSpaceEllipsoidal.java
+++ MOBAC/src/main/java/mobac/mapsources/mapspace/MercatorPower2MapSpaceEllipsoidal.java
@@ -28,18 +28,15 @@ import mobac.program.interfaces.MapSpace;
  * 
  * (C) 1999-2008, Open Source Geospatial Foundation (OSGeo)
  * 
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; version 2.1 of the License.
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; version 2.1 of the License.
  * 
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  * 
- * This package contains formulas from the PROJ package of USGS. USGS's work is
- * fully acknowledged here. This derived work has been relicensed under LGPL
- * with Frank Warmerdam's permission.
+ * This package contains formulas from the PROJ package of USGS. USGS's work is fully acknowledged here. This derived
+ * work has been relicensed under LGPL with Frank Warmerdam's permission.
  * 
  */
 public class MercatorPower2MapSpaceEllipsoidal extends MercatorPower2MapSpace {
@@ -55,9 +52,8 @@ public class MercatorPower2MapSpaceEllipsoidal extends MercatorPower2MapSpace {
 	private static final int MAXIMUM_ITERATIONS = 15;
 
 	/**
-	 * The square of excentricity: eÂ² = (aÂ²-bÂ²)/aÂ² where <var>e</var> is the
-	 * excentricity, <var>a</var> is the semi major axis length and <var>b</var>
-	 * is the semi minor axis length.
+	 * The square of excentricity: eÂ² = (aÂ²-bÂ²)/aÂ² where <var>e</var> is the excentricity, <var>a</var> is the semi
+	 * major axis length and <var>b</var> is the semi minor axis length.
 	 * 
 	 * For WGS84 ellipsoid a = 6378137 b = 6356752.3142
 	 */
@@ -65,8 +61,7 @@ public class MercatorPower2MapSpaceEllipsoidal extends MercatorPower2MapSpace {
 
 	/**
 	 * Ellipsoid excentricity, equals to <code>sqrt({@link
-	 * #excentricitySquared})</code>. Value 0 means that the ellipsoid is
-	 * spherical.
+	 * #excentricitySquared})</code>. Value 0 means that the ellipsoid is spherical.
 	 */
 	protected final double excentricity = Math.sqrt(excentricitySquared);
 
@@ -84,8 +79,7 @@ public class MercatorPower2MapSpaceEllipsoidal extends MercatorPower2MapSpace {
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see mobac.mapsources.mapspace.MercatorPower2MapSpace#cLatToY(double,
-	 * int)
+	 * @see mobac.mapsources.mapspace.MercatorPower2MapSpace#cLatToY(double, int)
 	 */
 	@Override
 	public int cLatToY(double lat, int zoom) {
@@ -126,8 +120,7 @@ public class MercatorPower2MapSpaceEllipsoidal extends MercatorPower2MapSpace {
 		double phi = (Math.PI / 2) - 2.0 * Math.atan(ts);
 		for (int i = 0; i < MAXIMUM_ITERATIONS; i++) {
 			final double con = excentricity * Math.sin(phi);
-			final double dphi = (Math.PI / 2) - 2.0
-					* Math.atan(ts * Math.pow((1 - con) / (1 + con), eccnth)) - phi;
+			final double dphi = (Math.PI / 2) - 2.0 * Math.atan(ts * Math.pow((1 - con) / (1 + con), eccnth)) - phi;
 			phi += dphi;
 			if (Math.abs(dphi) <= ITERATION_TOLERANCE) {
 				return phi;
@@ -138,15 +131,42 @@ public class MercatorPower2MapSpaceEllipsoidal extends MercatorPower2MapSpace {
 	}
 
 	/**
-	 * Computes function (15-9) and (9-13) from Snyder. Equivalent to negative
-	 * of function (7-7).
+	 * Computes function (15-9) and (9-13) from Snyder. Equivalent to negative of function (7-7).
 	 */
 	private double tsfn(final double phi, double sinphi) {
 		sinphi *= excentricity;
 		/*
 		 * NOTE: change sign to get the equivalent of Snyder (7-7).
 		 */
-		return Math.tan(0.5 * (Math.PI / 2 - phi))
-				/ Math.pow((1 - sinphi) / (1 + sinphi), 0.5 * excentricity);
+		return Math.tan(0.5 * (Math.PI / 2 - phi)) / Math.pow((1 - sinphi) / (1 + sinphi), 0.5 * excentricity);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		long temp;
+		temp = Double.doubleToLongBits(excentricity);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(excentricitySquared);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MercatorPower2MapSpaceEllipsoidal other = (MercatorPower2MapSpaceEllipsoidal) obj;
+		if (Double.doubleToLongBits(excentricity) != Double.doubleToLongBits(other.excentricity))
+			return false;
+		if (Double.doubleToLongBits(excentricitySquared) != Double.doubleToLongBits(other.excentricitySquared))
+			return false;
+		return true;
+	}
+
 }

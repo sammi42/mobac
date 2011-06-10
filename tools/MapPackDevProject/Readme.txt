fb7fe1c08216f2d67ca7c31f489b75d8c6def78b
r_x <r_x@83dc79ef-cac3-4f51-90f7-e9e68c987b9c>
2011-06-10 09:39:06 +0000

    git-svn-id: https://mobac.svn.sourceforge.net/svnroot/mobac/trunk@1819 83dc79ef-cac3-4f51-90f7-e9e68c987b9c

--- tools/MapPackDevProject/Readme.txt
+++
@@ -1,3 +0,0 @@
-Copy "Mobile_Atlas_Creator.jar" into the directory "lib", then clean&rebuild the project.
-
-For creating the MOBAc map pack(s) execute build.xml (Run As -> Ant Build).
---
+++ tools/MapPackDevProject/src/mobac/mapsources/mappacks/mymappack/MyMapsourceTemplate.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) MOBAC developers
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package mobac.mapsources.mappacks.mymappack;
+
+import java.net.HttpURLConnection;
+
+import mobac.mapsources.AbstractHttpMapSource;
+import mobac.program.download.UserAgent;
+import mobac.program.model.TileImageType;
+
+/**
+ * Example map source template.
+ */
+public class MyMapsourceTemplate extends AbstractHttpMapSource {
+
+	/**
+	 * Example URL - change to a valid base URL
+	 */
+	private static final String MAP_BASE_URL = "http://www.myosmtileserver.example/Tiles/tile";
+
+	public MyMapsourceTemplate() {
+		super("MyMapSource", 0, 17, TileImageType.PNG, TileUpdate.None);
+	}
+
+	@Override
+	public String getTileUrl(int zoom, int tilex, int tiley) {
+		return MAP_BASE_URL + "/" + zoom + "/" + tilex + "/" + tiley + ".png";
+	}
+
+	@Override
+	protected void prepareTileUrlConnection(HttpURLConnection conn) {
+		super.prepareTileUrlConnection(conn);
+		conn.setRequestProperty("User-agent", UserAgent.FF3_WIN7);
+	}
+
+	@Override
+	public String toString() {
+		return "My map source";
+	}
+
+}
